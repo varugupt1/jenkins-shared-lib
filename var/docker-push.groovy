6352de6b51c9b81b7521352fb34bfd8985b530b1
varugupt1 <47408645+varugupt1@users.noreply.github.com>
2025-09-19 04:59:54 +0530

    Update docker-push.groovy

--- var/docker-push.groovy
+++ var/docker-push.groovy
@@ -1,6 +1,6 @@
 def call(string project, string tag, string credentialsId ){
   withCredentials([usernamePassword(credentialsId: 'DockerhubCred', passwordVariable: 'passwd', usernameVariable: 'username')]) {
   sh 'docker login -u $username -p $passwd'
-  sh 'docker tag notes-app:latest $username/notes-app:latest'
-  sh 'docker push $username/notes-app:latest'
+  sh 'docker tag $project:$tag $username/$project:$tag'
+  sh 'docker push $username/$project:latest'
 }
