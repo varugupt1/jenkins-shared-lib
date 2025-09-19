db45ee7a0695653eb061ed6e3fcd559ba36adb15
varugupt1 <47408645+varugupt1@users.noreply.github.com>
2025-09-19 06:06:09 +0530

    Update push.groovy

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,4 +1,4 @@
-def call(string project, string tag, string credentialsId ){
+def call(String project, String tag, String credentialsId ){
   withCredentials([usernamePassword(credentialsId: ${credentialsId}, passwordVariable: 'passwd', usernameVariable: 'username')]) {
   sh 'docker login -u $username -p $passwd'
   sh 'docker tag ${project}:${tag} $username/${project}:${tag}'
